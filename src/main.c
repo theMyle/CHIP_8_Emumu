d9b0fcf9ca5b8564b32985f7aae741af4f784c29
John Kyle Desamparo <99082612+theMyle@users.noreply.github.com>
2025-05-25 07:50:27 +0800

    CHIP 8 Base structure

--- src/main.c
+++ src/main.c
@@ -1,32 +1,14 @@
 #include <stdio.h>
-#include <stdint.h>
+#include "chip8.h"
 
-// register names
-enum Registers {
-  V0 = 0,
-  V1 = 1,
-  V2 = 2,
-  V3 = 3,
-  V4 = 4,
-  V5 = 5,
-  V6 = 6,
-  V7 = 7,
-  V8 = 8,
-  V9 = 9,
-  VA = 10,
-  VB = 11,
-  VC = 12,
-  VD = 13,
-  VE = 14,
-  VF = 15,
-};
+// instantiate the chip8 system
+Chip8 system;
 
-// register
-uint8_t registers[16];
-
-int main() {
-  printf("Hello World\n");
-  registers[V0] = 255;
-  printf("%d %d\n", registers[V0], registers[V1]);
+int main()
+{
+  for (int i = 0; i < sizeof(system.registers) / sizeof(system.registers[0]); i++)
+  {
+    printf("Register[%d] = %d\n", i, system.registers[i]);
+  }
   return 0;
 }
